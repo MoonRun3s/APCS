4f6a86310cd0598da46ec693a2fdde984fed6717
jrich <68448908+MoonRun3s@users.noreply.github.com>
2022-11-14 13:49:51 -0500

    Create Main.java

---
+++ projects/Wordle/Main.java
@@ -0,0 +1,24 @@
+// group: jad richani, layla agamy, vincent shi, alan achilles
+
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Wordle wordle = new Wordle();
+    Scanner keyboardScanner = new Scanner(System.in);
+
+    wordle.printResult(false);
+    while (wordle.guessesRemaining > 0) {
+      String guessedWord = keyboardScanner.next();
+      guessedWord = guessedWord.toLowerCase();
+      if (guessedWord.length() == 5) {
+        wordle.checkWord(guessedWord); 
+      } else {
+        wordle.printResult(false);
+        System.out.println("Not a valid guess, try again.");
+      }
+    }
+    System.out.println("Game over, the word was \"" + wordle.getWord() + ".\"");
+    System.exit(130);
+  }
+}
